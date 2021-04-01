4fea86c00822189dfd8637e29069a9a21a47d6b1
Jitendra Kumar Patra <40471061+patrajitendra@users.noreply.github.com>
2021-04-01 12:43:35 +0530

    Update settings.py

--- TranslatorApp/TranslatorApp/settings.py
+++ TranslatorApp/TranslatorApp/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'Translator',
-        'HOST' : '127.0.0.1',
+        'HOST' : 'localhost',
         'PORT' : '3306',
         'PASSWORD' : '1234jitu',
         'USER' : 'root',
